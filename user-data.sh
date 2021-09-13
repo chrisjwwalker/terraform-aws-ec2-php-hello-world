d4fb3780ef7600a1be403e73ee72366d52e5000e
Chris J W Walker <8091191+chrisjwwalker@users.noreply.github.com>
2021-09-13 11:54:14 +0100

    Change to the user data script

--- user-data.sh
+++ user-data.sh
@@ -2,6 +2,8 @@
 
 exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
+echo "Hello from the user data script!"
+
 sudo amazon-linux-extras enable nginx1
 sudo yum clean metadata
 sudo yum install -y nginx
